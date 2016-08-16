fbd28f5f4c31973210bdacf5a495e94196df5187
Lai <lai32690@gmail.com>
2016-08-16 20:17:53 -0300

    add scene load event

---
+++ Assets/scripts/BasicCommands.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BasicCommands : MonoBehaviour {
+
+    public void loadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
