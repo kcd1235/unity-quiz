69acae80f8360515e31f961255be2d98e09664ec
Lai <lai32690@gmail.com>
2016-08-16 14:24:44 -0300

    add background animation

---
+++ Assets/scripts/MoveOffset.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveOffset : MonoBehaviour {
+
+	[SerializeField]
+	public float horizontalSpeed;
+	[SerializeField]
+	public float verticalSpeed;
+
+	private Material material;
+	private float horizontalOffset = 1;
+	private float verticalOffset = 1;
+
+
+	// Use this for initialization
+	void Start () {
+		material = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate(){
+		horizontalOffset += 1;
+		verticalOffset += 1;
+
+		Vector2 vector = new Vector2 (horizontalOffset * horizontalSpeed, verticalOffset * verticalSpeed);
+		material.SetTextureOffset ("_MainTex", vector);
+	}
+
+	void Update () {
+	}
+}
